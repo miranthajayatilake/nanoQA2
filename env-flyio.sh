b8e2a32d13c742fafe40abf78e5c37af884e7066
mirantha <mj.jayathilaka@gmail.com>
2023-04-15 17:46:01 +0530

    upload env files

---
+++ env-flyio.sh
@@ -0,0 +1,6 @@
+flyctl secrets set INDEX_NAME="asdf" \
+PINECONE_API_KEY="asdf" \
+PINECONE_ENV="asfd" \
+NAMESPACE="asdf" \
+OPENAI_API_KEY="asdf" \
+EGPTNAME="asdf" SQone="asdf" SQtwo="asdf" SQthree="asdf" 
---
+++ env-local.sh
@@ -0,0 +1,6 @@
+export INDEX_NAME "asdf" \
+PINECONE_API_KEY "asdf" \
+PINECONE_ENV "asdf" \
+NAMESPACE "asdf" \
+OPENAI_API_KEY "asdf" \
+EGPTNAME "asdf" SQone "asdf" SQtwo "asdf" SQthree "asdf"
